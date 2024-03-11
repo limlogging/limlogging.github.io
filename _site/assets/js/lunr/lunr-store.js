ad977a5ec81d5b979e2ff7967020c4c4d87b9766
limlogging <limlogging@gmail.com>
2024-03-11 10:45:34 +0900

    알고리즘문제풀이추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -340,4 +340,10 @@ var store = [{
         "tags": ["TIL"],
         "url": "/TIL/til9/",
         "teaser": null
+      },{
+        "title": "[프로그래머스/Swift] 크기가 작은 부분 문자열",
+        "excerpt":"문제 설명 숫자로 이루어진 문자열 t와 p가 주어질 때, t에서 p와 길이가 같은 부분문자열 중에서, 이 부분문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수를 return하는 함수 solution을 완성하세요. 예를 들어, t=”3141592”이고 p=”271” 인 경우, t의 길이가 3인 부분 문자열은 314, 141, 415, 159, 592입니다. 이 문자열이 나타내는...","categories": ["Algorithm"],
+        "tags": ["Algorithm","coding test","크기가 작은 부분 문자열"],
+        "url": "/algorithm/%ED%81%AC%EA%B8%B0%EA%B0%80%EC%9E%91%EC%9D%80%EB%B6%80%EB%B6%84%EB%AC%B8%EC%9E%90%EC%97%B4/",
+        "teaser": null
       }]
