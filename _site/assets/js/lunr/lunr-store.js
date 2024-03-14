c98ef1bda4540aa0942d8ba50d3c14510152c2fc
limlogging <limlogging@gmail.com>
2024-03-14 11:02:53 +0900

    숫잠분자열과영단어 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -388,4 +388,10 @@ var store = [{
         "tags": ["TIL"],
         "url": "/TIL/til11/",
         "teaser": null
+      },{
+        "title": "[프로그래머스/Swift] 숫자 문자열과 영단어",
+        "excerpt":"문제 설명 네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다. 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다. 1478 → “one4seveneight” 234567 → “23four5six7” 10203 → “1zerotwozero3” 이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열...","categories": ["Algorithm"],
+        "tags": ["Algorithm","coding test","숫자 문자열과 영단어"],
+        "url": "/algorithm/%EC%88%AB%EC%9E%90%EB%AC%B8%EC%9E%90%EC%97%B4%EA%B3%BC%EC%98%81%EB%8B%A8%EC%96%B4/",
+        "teaser": null
       }]
