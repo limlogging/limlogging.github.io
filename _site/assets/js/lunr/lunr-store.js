4bc9a43455375d880c133329f245c60d9f8913c2
imhs <limlogging@gmail.com>
2024-03-19 13:28:17 +0900

    알고리즘-문자열내맘대로정렬하기추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -520,4 +520,10 @@ var store = [{
         "tags": ["TIL"],
         "url": "/TIL/til15/",
         "teaser": null
+      },{
+        "title": "[프로그래머스/Swift] 문자열 내 마음대로 정렬하기",
+        "excerpt":"문제 설명 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 예를 들어 strings가 [“sun”, “bed”, “car”]이고 n이 1이면 각 단어의 인덱스 1의 문자 “u”, “e”, “a”로 strings를 정렬합니다. 문제 풀기 제한사항 strings는 길이 1 이상, 50이하인 배열입니다. strings의 원소는 소문자 알파벳으로...","categories": ["Algorithm"],
+        "tags": ["Algorithm","coding test","문자열 내 마음대로 정렬하기"],
+        "url": "/algorithm/%EB%AC%B8%EC%9E%90%EC%97%B4%EB%82%B4%EB%A7%88%EC%9D%8C%EB%8C%80%EB%A1%9C%EC%A0%95%EB%A0%AC%ED%95%98%EA%B8%B0/",
+        "teaser": null
       }]
