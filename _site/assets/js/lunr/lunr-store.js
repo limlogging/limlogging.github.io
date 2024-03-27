9a02581ce46ba8039e6162ee7325864ef5b78106
imhs <limlogging@gmail.com>
2024-03-27 11:00:54 +0900

    알고리즘풀이추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -658,4 +658,10 @@ var store = [{
         "tags": ["swift","iOS","TableView","moveRowAt"],
         "url": "/uikit/%EB%AA%A9%EB%A1%9D%EC%88%9C%EC%84%9C%EB%B0%94%EA%BE%B8%EA%B8%B0/",
         "teaser": null
+      },{
+        "title": "[프로그래머스/Swift] 2016년",
+        "excerpt":"문제 설명 2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT 입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 “TUE”를 반환하세요. 문제 풀기 제한사항 2016년은 윤년입니다. 2016년 a월 b일은...","categories": ["Algorithm"],
+        "tags": ["Algorithm","coding test","2016년"],
+        "url": "/algorithm/2016%EB%85%84/",
+        "teaser": null
       }]
