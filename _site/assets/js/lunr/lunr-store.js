739d421338686f622d638f5ce8677caf57d55012
HyeongSub Lim <limlogging@gmail.com>
2024-04-01 22:44:24 +0900

    알고리즘풀이추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -712,4 +712,10 @@ var store = [{
         "tags": ["TIL"],
         "url": "/TIL/til24/",
         "teaser": null
+      },{
+        "title": "[프로그래머스/Swift] 모의고사",
+        "excerpt":"문제 설명 수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다. 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, … 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5,...","categories": ["Algorithm"],
+        "tags": ["Algorithm","coding test","모의고사"],
+        "url": "/algorithm/%EB%AA%A8%EC%9D%98%EA%B3%A0%EC%82%AC/",
+        "teaser": null
       }]
